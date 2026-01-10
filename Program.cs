66cdf86ae87006993d19041491268226ecc207a4
Monsupito8 <agaevgulam08082000@mail.ru>
2026-01-11 00:56:42 +0300

    Удалил PostgreSQL, оставил только SQLite

--- Program.cs
+++ Program.cs
@@ -6,34 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Добавляем поддержку контроллеров
 builder.Services.AddControllers();
 
-// Настройка базы данных
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-// Если на Railway есть PostgreSQL, используем его
-var railwayDbUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-if (!string.IsNullOrEmpty(railwayDbUrl))
-{
-    // Конвертируем URL из Railway в строку подключения Npgsql
-    var uri = new Uri(railwayDbUrl);
-    var db = uri.AbsolutePath.Trim('/');
-    var user = uri.UserInfo.Split(':')[0];
-    var passwd = uri.UserInfo.Split(':')[1];
-    var dbPort = uri.Port > 0 ? uri.Port : 5432; // ИЗМЕНИЛ НА dbPort
-    
-    connectionString = $"Host={uri.Host};Port={dbPort};Database={db};Username={user};Password={passwd};SSL Mode=Require;Trust Server Certificate=true";
-    
-    builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseNpgsql(connectionString));
-}
-else
-{
-    // Для локальной разработки используем SQLite
-    builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlite("Data Source=iptracker.db"));
-}
+// ТОЛЬКО SQLite - убрал весь PostgreSQL код
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite("Data Source=iptracker.db"));
 
 // Настраиваем порт
-var appPort = Environment.GetEnvironmentVariable("PORT") ?? "8080"; // ИЗМЕНИЛ НА appPort
+var appPort = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 builder.WebHost.UseUrls($"http://0.0.0.0:{appPort}");
 
 var app = builder.Build();
@@ -46,5 +24,15 @@ using (var scope = app.Services.CreateScope())
 }
 
 app.MapControllers();
-app.MapGet("/", () => "IP Tracker работает!");
+
+// Главная страница
+app.MapGet("/", () => @"<!DOCTYPE html>
+<html>
+<head><title>IP Tracker</title></head>
+<body>
+<h1>✅ IP Tracker работает!</h1>
+<p>API доступно по адресу: /api/tracker/generate</p>
+</body>
+</html>");
+
 app.Run();
